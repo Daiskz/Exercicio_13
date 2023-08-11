d1f7bf8b01d103330b9f2f16981ff9e1c59fcd19
gabriel.matsubara@gmail.com <gabriel.matsubara@gmail.com>
2023-08-11 16:11:58 -0300

    Mudanças no projeto

--- Exercicio_13/README.md
+++ Exercicio_13/README.md
@@ -1,12 +1,14 @@
 ## Projeto Modelo para Atividade Java X GitHub
 
-Sejam bem-vindos ao projeto
+Bem vindo ao projeto!
 
 ## Enunciado
 
 Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
-A - Para homens: (72.7*h) - 58
-B - Para mulheres: (62.1*h) - 44.7
+
+A - Para homens: "(72.7*h) - 58".
+
+B - Para mulheres: "(62.1*h) - 44.7".
 
 ## Dependency Management
 
--- Exercicio_13/src/App.java
+++ Exercicio_13/src/App.java
@@ -4,11 +4,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         try (Scanner entrada = new Scanner(System.in)) {
             System.out.println("Digite a sua altura: ");
-            double a = entrada.nextDouble();
-            double h = ((72.7 * a) - 58);
-            double m = ((62.1 * a) - 44.7);
-   System.out.println("O peso ideal para homem é: " + h + "KG");
-   System.out.println("O peso ideal para mulher é: " + m + "KG");
+            double altura = entrada.nextDouble();
+            double homem = ((72.7 * altura) - 58);
+            double mulher = ((62.1 * altura) - 44.7);
+   System.out.println("O peso ideal para homem é: " + homem + "KG");
+   System.out.println("O peso ideal para mulher é: " + mulher + "KG");
         }
     }
 }
